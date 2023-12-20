8c514ad643a27b04bd5ad821414df06bc992adf9
rathoredev32-ui <126143890+rathoredev32-ui@users.noreply.github.com>
2023-12-20 16:36:14 +0530

    Dom File

--- domtask4/dom.js
+++ domtask4/dom.js
@@ -1,1 +1,6 @@
-console.log('good morning');
+const fruit = document.getElementsByClassName('fruit');
+console.log(fruit[2]);
+fruit[2].style.backgroundColor='yellow';
+for(let i=0; i<fruit.length; i++){
+  fruit[i].style.fontWeight='bold';
+}
