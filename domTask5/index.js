9aa1c410133faf9f56c4b6b0cfa19223aeecd6f4
rathoredev32-ui <126143890+rathoredev32-ui@users.noreply.github.com>
2023-12-20 16:52:55 +0530

    Task5

---
+++ domTask5/index.js
@@ -0,0 +1,6 @@
+// Write your code below:
+const listItem=document.getElementsByTagName('li');
+listItem[4].style.color='blue';
+for(let i=0; i<listItem.length; i++){
+  listItem[i].style.fontStyle='italic';
+}
